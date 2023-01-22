5ddf9f68a0f3a88f498268c620dc6711d6b9c306
priya.shankar <priya.shankar@kosoft.co>
2023-01-22 08:39:39 +0530

    Initial Commit

---
+++ ExpenseTrackr/ExpenseTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackrApp.swift
+//  ExpenseTrackr
+//
+//  Created by Priya Shankar on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
